d06b572508f6fcaf0cc1ab43117f4475f5505cd3
gp_aranha <gianp_aranha@hotmail.com>
2024-09-23 21:25:49 -0300

    Add credentials database migration

---
+++ api/src/main/resources/db/migration/V5__create-table-credentials.sql
@@ -0,0 +1,9 @@
+CREATE TABLE credentials(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    user_id BIGINT NOT NULL,
+
+    PRIMARY KEY(id),
+    CONSTRAINT fk_credentials_user_id FOREIGN KEY(user_id) REFERENCES users(id)
+);
