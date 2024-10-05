4f11a2bc23dae963c850872af57cd95bd454574e
Paulo Vítor Reis <129749259+paulovreis@users.noreply.github.com>
2024-10-05 11:26:15 -0300

    Correção de erros e testes no postman finalizados

--- api/src/main/java/br/com/cineroom/api/dtos/session/SessionDTO.java
+++ api/src/main/java/br/com/cineroom/api/dtos/session/SessionDTO.java
@@ -3,23 +3,26 @@ package br.com.cineroom.api.dtos.session;
 import br.com.cineroom.api.entities.User;
 import br.com.cineroom.api.utilities.Category;
 import br.com.cineroom.api.utilities.Status;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record SessionDTO(
     @NotBlank
     String code,
-    @NotBlank
+    @NotNull(message = "Category cannot be null")
     Category category,
-    @NotBlank
+    @NotNull(message = "userLimit cannot be null")
     Integer usersLimit,
-    @NotBlank
+    @NotNull(message = "Status cannot be null")
     Status status,
     @NotBlank
     String content,
-    @NotBlank
+    @Nullable
     LocalDateTime createdAt,
-    @NotBlank
+    @NotNull(message = "User cannot be null")
     User user
 ){
 }
